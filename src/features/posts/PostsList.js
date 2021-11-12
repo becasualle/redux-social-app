f609522d327a6e060797e94ecca3d7c382537266
Mark <mark.nelubin@gmail.com>
2021-11-12 16:32:58 +0300

    Sorting the Posts List

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,13 +3,18 @@ import { useSelector } from 'react-redux'
 import { selectPosts } from './postSlice'
 import { Link } from 'react-router-dom'
 import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
     const posts = useSelector(selectPosts);
-
-    const renderPosts = posts.map(post => (
+    // In order to show newest posts first, we have to sort our posts by timestamp
+    // Since array.sort() mutates the existing array, we need to make a copy of state.posts and sort that copy
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    console.log(orderedPosts)
+    const renderPosts = orderedPosts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <div>
+                <TimeAgo timestamp={post.date} />
                 <PostAuthor userId={post.user} />
             </div>
             <h3>{post.title}</h3>
